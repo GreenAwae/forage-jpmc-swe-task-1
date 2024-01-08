970f4230d50d69a218a77f2149bfc6169afdba81
GreenAwae <elnaw.awae@gmail.com>
2024-01-08 11:26:05 -0500

    Task 1 complete w/ Optional Unit Tests

---
+++ forage-jpmc-swe-task-1-main/client_test.py
@@ -0,0 +1,41 @@
+import unittest
+from client3 import getDataPoint
+from client3 import getRatio
+
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
+
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) /2))
+
+
+  """ ------------ Add more unit tests ------------ """
+
+""" ------ was suggested to try ratio"""
+  def test_getRatio_notZero(self):
+    prices = [
+      {'price_a': 10, 'price_b': 12},
+      {'price_a': 45, 'price_b': 23},
+      {'price_a': 119.24, 'price_b': 124.13}
+    ]
+
+    for price in prices:
+      self.assertEqual(getRatio(price['price_a'],price['price_b']), price['price_a']/price['price_b'])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
